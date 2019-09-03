b93823a3677302099edc9a1e7944cfbe4afc3322
Bartłomiej Kluska <piaskunbk@gmail.com>
2019-09-03 19:55:35 +0200

    added logging, added multi user functionality

--- index.php
+++ index.php
@@ -2,21 +2,19 @@
 error_reporting(1);
 ini_set('display_errors', 'On');
 ini_set('display_startup_errors', 'On');
-
 require 'creds.php';
 
 $conn = new mysqli($servername, $username, $password, $dbname);
 if ($conn->connect_error) {
-//	echo $username;
-//	echo $password;
 	die("INTERNAL ERROR");
 }
 $pass = $_POST['Password'];
+$mac = $_POST['MAC'];
 $sensor_ID = 0;
 $result = $conn->query("SELECT Sensor_Id,Sensor_Name,Mac_Address,Password FROM TemperatureMeasureSite.Sensors");
 if ($result->num_rows > 0) {
     while($row = $result->fetch_assoc()){
-	if($row["Mac_Address"] == $_POST['MAC'] && $row["Password"] == $pass)
+	if($row["Mac_Address"] == $mac && $row["Password"] == $pass)
         {
         	$sensor_ID = $row["Sensor_Id"];
         	break;
@@ -75,44 +73,38 @@ if($sensor_ID != 0)
 	} else {
 	    echo "Error: " . $sql . "<br>" . $conn->error;
 	}
+} else {
+	$sql = "INSERT INTO TemperatureMeasureSite.Sensor_Log (
+		Sensor_ID,
+		AVG_Humidity,
+		Max_Humidity,
+		Min_Humidity,
+		AVG_Temperature,
+		Max_Temperature,
+		Min_Temperature,
+		Mac_Address,
+		Password
+		)
+		VALUES (
+		$sensor_ID,
+		$AVG_Humidity,
+		$Max_Humidity,
+		$Min_Humidity,
+		$AVG_Temperature,
+		$Max_Temperature,
+		$Min_Temperature,
+		$mac,
+		$pass
+	)";
+	if ($conn->query($sql) === TRUE) {
+		echo "New record created successfully";
+	} else {
+		echo "Error: " . $sql . "<br>" . $conn->error;
+	}
 }
 
-if( $_GET["action"] == "DISP" )
-{
-	if(!empty($_GET['from']))
-	{
-		$timestamp2 = date($_GET['from']);
-		$result = $conn->query("select ID,Sensors.Sensor_Name,Timestamp_Of_Reading,AVG_Humidity,Max_Humidity,Min_Humidity, AVG_Temperature, Max_Temperature, Min_Temperature 
-	from Sensor_Readings
-	inner join Sensors
-	on Sensor_Readings.Sensor_ID = Sensors.Sensor_Id
-	where Sensor_Readings.Timestamp_Of_Reading >= "."'".$timestamp2."';");
-		$resultArray = array();
-		if ($result->num_rows > 0) {
-		    while($row = $result->fetch_assoc()) {
-			$resultArray[] = $row;
-		   }
-		}
-		echo json_encode($resultArray);
-	}
-	else
-	{
-		$result = $conn->query("select * from (
-			select ID,Sensors.Sensor_Name,Timestamp_Of_Reading,AVG_Humidity,Max_Humidity,Min_Humidity, AVG_Temperature, Max_Temperature,Min_Temperature
-			from Sensor_Readings
-			inner join Sensors
-			on Sensor_Readings.Sensor_ID = Sensors.Sensor_Id
-			order by ID desc limit 500) sub
-			order by ID asc");
-		$resultArray = array();
-		if ($result->num_rows > 0) {
-		    while($row = $result->fetch_assoc()) {
-			$resultArray[] = $row;
-		   }
-		}
-		echo json_encode($resultArray);
-	}
-}
+
+
 
 if($_GET['action'] == 'LAST')
 {
@@ -125,6 +117,22 @@ if($_GET['action'] == 'LAST')
 	}
         echo json_encode($resultArray);
 }
+
+if($_GET['action'] == 'MyLasts')
+{
+	$UserName = $_POST['UserName'];
+	$ApiKey = $_POST['ApiKey'];
+	
+	$result = $conn->query("CALL Users_Last($UserName, $ApiKey)");
+        $resultArray = array();
+        if ($result->num_rows > 0) {
+		while($row = $result->fetch_assoc()) {
+                	$resultArray[] = $row;
+                }
+	}
+        echo json_encode($resultArray);
+}
+
 
 
 $conn->close();
