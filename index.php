85d15c77f92754c151a84b47daed1640ceff06bb
Bartłomiej Kluska <piaskunbk@gmail.com>
2019-10-20 18:54:38 +0200

    added new features, fixed few errors

--- index.php
+++ index.php
@@ -126,15 +126,107 @@ if($_GET['action'] == 'MyLasts')
 {
 	$UserName = $_GET['UserName'];
 	$ApiKey = $_GET['ApiKey'];
-	$sql = "CALL `xd`('".$ApiKey."','".$UserName."', @Xxxd );";
-	$result = $conn->query($sql);
-	$sql = "select @Xxxd";
-	$result = $conn->query($sql);
-        $resultArray = array();
-        if ($result->num_rows > 0) {
-	$row = $result->fetch_row();
-	echo $row[0];
+//	$queryName = "xd";
+	$sql = "SELECT *
+			FROM (
+			SELECT `temptable1`.`ID` AS `ID`,
+			`temptable1`.`Sensor_Name` AS `Sensor_Name`, CONCAT(IF((HOUR(`temptable1`.`diffTime`) > 0), CONCAT(HOUR(`temptable1`.`diffTime`),' godzin '),''), IF((MINUTE(`temptable1`.`diffTime`) > 0), CONCAT(MINUTE(`temptable1`.`diffTime`),' minut '),''), SECOND(`temptable1`.`diffTime`),' sekund temu') AS `Timestamp_Of_Reading`,
+			`temptable1`.`AVG_Humidity` AS `AVG_Humidity`,
+			`temptable1`.`Max_Humidity` AS `Max_Humidity`,
+			`temptable1`.`Min_Humidity` AS `Min_Humidity`,
+			`temptable1`.`AVG_Temperature` AS `AVG_Temperature`,
+			`temptable1`.`Max_Temperature` AS `Max_Temperature`,
+			`temptable1`.`Min_Temperature` AS `Min_Temperature`
+			FROM (
+			SELECT `ttable`.`ID` AS `ID`,
+			`ttable`.`Sensor_Name` AS `Sensor_Name`,
+			`ttable`.`Timestamp_Of_Reading` AS `Timestamp_Of_Reading`,
+			`ttable`.`AVG_Humidity` AS `AVG_Humidity`,
+			`ttable`.`Max_Humidity` AS `Max_Humidity`,
+			`ttable`.`Min_Humidity` AS `Min_Humidity`,
+			`ttable`.`AVG_Temperature` AS `AVG_Temperature`,
+			`ttable`.`Max_Temperature` AS `Max_Temperature`,
+			`ttable`.`Min_Temperature` AS `Min_Temperature`, SEC_TO_TIME((UNIX_TIMESTAMP(NOW()) - UNIX_TIMESTAMP(`ttable`.`Timestamp_Of_Reading`))) AS `diffTime`
+			FROM (
+			SELECT *
+			FROM RAW_Last_Measurements rlm
+			JOIN Users us ON rlm.Sensor_Owner = us.User_ID
+			WHERE us.UserName = '".$UserName."' AND us.API_KEY = '".$ApiKey."') `ttable`
+						) `temptable1`
+			WHERE ((MINUTE(`temptable1`.`diffTime`) < 5) AND (HOUR(`temptable1`.`diffTime`) = 0))
+			ORDER BY `temptable1`.`diffTime`) ttj;";
+	switch ($_GET['ApiVersion']) {
+		case '1.1':
+			$sql = "SELECT *
+			FROM (
+			SELECT `temptable1`.`ID` AS `ID`,
+			`temptable1`.`Sensor_Name` AS `Sensor_Name`, 
+			`temptable1`.`Timestamp_Of_Reading` AS `Timestamp_Of_Reading`,
+			`temptable1`.`AVG_Humidity` AS `AVG_Humidity`,
+			`temptable1`.`Max_Humidity` AS `Max_Humidity`,
+			`temptable1`.`Min_Humidity` AS `Min_Humidity`,
+			`temptable1`.`AVG_Temperature` AS `AVG_Temperature`,
+			`temptable1`.`Max_Temperature` AS `Max_Temperature`,
+			`temptable1`.`Min_Temperature` AS `Min_Temperature`
+			FROM (
+			SELECT `ttable`.`ID` AS `ID`,
+			`ttable`.`Sensor_Name` AS `Sensor_Name`,
+			`ttable`.`Timestamp_Of_Reading` AS `Timestamp_Of_Reading`,
+			`ttable`.`AVG_Humidity` AS `AVG_Humidity`,
+			`ttable`.`Max_Humidity` AS `Max_Humidity`,
+			`ttable`.`Min_Humidity` AS `Min_Humidity`,
+			`ttable`.`AVG_Temperature` AS `AVG_Temperature`,
+			`ttable`.`Max_Temperature` AS `Max_Temperature`,
+			`ttable`.`Min_Temperature` AS `Min_Temperature`, SEC_TO_TIME((UNIX_TIMESTAMP(NOW()) - UNIX_TIMESTAMP(`ttable`.`Timestamp_Of_Reading`))) AS `diffTime`
+			FROM (
+			SELECT *
+			FROM RAW_Last_Measurements rlm
+			JOIN Users us ON rlm.Sensor_Owner = us.User_ID
+			WHERE us.UserName = '".$UserName."' AND us.API_KEY = '".$ApiKey."') `ttable`
+						) `temptable1`
+			WHERE ((MINUTE(`temptable1`.`diffTime`) < 5) AND (HOUR(`temptable1`.`diffTime`) = 0))
+			ORDER BY `temptable1`.`diffTime`) ttj;";
+			break;
+		
+		default:
+			$sql = "SELECT *
+			FROM (
+			SELECT `temptable1`.`ID` AS `ID`,
+			`temptable1`.`Sensor_Name` AS `Sensor_Name`, CONCAT(IF((HOUR(`temptable1`.`diffTime`) > 0), CONCAT(HOUR(`temptable1`.`diffTime`),' godzin '),''), IF((MINUTE(`temptable1`.`diffTime`) > 0), CONCAT(MINUTE(`temptable1`.`diffTime`),' minut '),''), SECOND(`temptable1`.`diffTime`),' sekund temu') AS `Timestamp_Of_Reading`,
+			`temptable1`.`AVG_Humidity` AS `AVG_Humidity`,
+			`temptable1`.`Max_Humidity` AS `Max_Humidity`,
+			`temptable1`.`Min_Humidity` AS `Min_Humidity`,
+			`temptable1`.`AVG_Temperature` AS `AVG_Temperature`,
+			`temptable1`.`Max_Temperature` AS `Max_Temperature`,
+			`temptable1`.`Min_Temperature` AS `Min_Temperature`
+			FROM (
+			SELECT `ttable`.`ID` AS `ID`,
+			`ttable`.`Sensor_Name` AS `Sensor_Name`,
+			`ttable`.`Timestamp_Of_Reading` AS `Timestamp_Of_Reading`,
+			`ttable`.`AVG_Humidity` AS `AVG_Humidity`,
+			`ttable`.`Max_Humidity` AS `Max_Humidity`,
+			`ttable`.`Min_Humidity` AS `Min_Humidity`,
+			`ttable`.`AVG_Temperature` AS `AVG_Temperature`,
+			`ttable`.`Max_Temperature` AS `Max_Temperature`,
+			`ttable`.`Min_Temperature` AS `Min_Temperature`, SEC_TO_TIME((UNIX_TIMESTAMP(NOW()) - UNIX_TIMESTAMP(`ttable`.`Timestamp_Of_Reading`))) AS `diffTime`
+			FROM (
+			SELECT *
+			FROM RAW_Last_Measurements rlm
+			JOIN Users us ON rlm.Sensor_Owner = us.User_ID
+			WHERE us.UserName = '".$UserName."' AND us.API_KEY = '".$ApiKey."') `ttable`
+						) `temptable1`
+			WHERE ((MINUTE(`temptable1`.`diffTime`) < 5) AND (HOUR(`temptable1`.`diffTime`) = 0))
+			ORDER BY `temptable1`.`diffTime`) ttj;";
+			break;
 	}
+	$result = $conn->query($sql);
+	$resultArray = array();
+	if ($result->num_rows > 0) {
+		while($row = $result->fetch_assoc()) {
+			$resultArray[] = $row;
+		}
+	}
+	echo json_encode($resultArray);	
 }
 if($_GET['action'] == 'validate')
 {
@@ -154,8 +246,68 @@ if($_GET['action'] == 'validate')
 	}
 	echo json_encode($UserValidation);
 }
+if($_GET['action'] == 'showSensors')
+{
+	$UserName = $_GET['UserName'];
+	$ApiKey = $_GET['ApiKey'];
+	$sql = "SELECT Sensor_Id,Sensor_Name
+	FROM Sensors ss
+	JOIN Users us
+	ON ss.User_ID = us.User_ID
+	WHERE us.UserName = '".$UserName."'
+	AND us.API_KEY = '".$ApiKey."'";
+	$result = $conn->query($sql);
+	$resultArray = array();
+	if ($result->num_rows > 0) {
+		while($row = $result->fetch_assoc()) {
+			$resultArray[] = $row;
+		}
+	}
+	echo json_encode($resultArray);
+}
+if($_GET['action'] == 'showSensorAveraged')
+{
+	$startDate = $_GET['StartDate'];
+	$endDate = $_GET['EndDate'];
+	$sensor_ID = $_GET['Sensor_ID'];
+
+	$sql = "SELECT 
+	  'dateDay',ttj.dateDay,
+	  'dateHour',ttj.dateHour,
+	  'AVG_Temperature',ttj.AVG_Temperature,
+	  'AVG_Humidity',ttj.AVG_Humidity
+from (
+select
+	date(Timestamp_Of_Reading) dateDay,
+ 	(hour(Timestamp_Of_Reading)) dateHour,
+   avg(AVG_Temperature) AVG_Temperature,
+	avg(AVG_Humidity) AVG_Humidity
+FROM (
+	SELECT Timestamp_Of_Reading,AVG_Temperature,AVG_Humidity
+	FROM Sensor_Readings sr
+	WHERE 
+		sr.Timestamp_Of_Reading >= '".$startDate."' AND
+		sr.Timestamp_Of_Reading <= '".$endDate."' and
+		sr.Sensor_ID = '".$sensor_ID."'
+	) xd
+group by date(Timestamp_Of_Reading),(hour(Timestamp_Of_Reading))
+ORDER BY dateDay,dateHour DESC
+) ttj";
+	$result = $conn->query($sql);
+	$resultArray = array();
+	if ($result->num_rows > 0) {
+		while($row = $result->fetch_assoc()) {
+			$resultArray[] = $row;
+		}
+	}
+	echo json_encode($resultArray);
+}
+
+
+
 
 $conn->close();
 
 ?>
+
 
