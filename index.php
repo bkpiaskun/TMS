087ab39cf1c4c604affa609658c2b78247a03888
Bartlomiej Kluska <piaskunbk@gmail.com>
2020-07-09 22:12:52 +0100

    added CORS header

--- index.php
+++ index.php
@@ -1,9 +1,12 @@
 <?php
+header('Access-Control-Allow-Origin: *');
 
 error_reporting(1);
 ini_set('display_errors', 'On');
 ini_set('display_startup_errors', 'On');
 require 'creds.php';
+
+
 
 $conn = new mysqli($servername, $username, $password, $dbname);
 if ($conn->connect_error) {
