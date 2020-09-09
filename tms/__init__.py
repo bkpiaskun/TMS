8061164a201fc88264454bf540461ee0d6151cca
Bartłomiej Kluska <piaskunbk@gmail.com>
2020-09-09 20:09:56 +0200

    Initial commit

---
+++ tms/__init__.py
@@ -0,0 +1,16 @@
+from flask import Flask, Response, jsonify
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+@app.errorhandler(Exception)
+def handle_exception(e):
+    data_dict = [{"status": "Failure"}]
+    response = jsonify(data_dict)
+    response.headers["Access-Control-Allow-Origin"] = "*"
+    return response
+
+app.config['SECRET_KEY'] = 'aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://USER:PASSWORD@IPADDRESS:5432/TMS'
+db = SQLAlchemy(app)
+
+from tms import routes
