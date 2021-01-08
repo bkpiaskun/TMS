9d0900bb9c0cc0c7fafe91361778d262adddd809
Bartłomiej Kluska <piaskunbk@gmail.com>
2021-01-08 19:45:44 +0100

    changed esp32 ds18b20 backend

--- esp32_ds18b20/esp32_ds18b20_TMS.ino
+++ esp32_ds18b20/esp32_ds18b20_TMS.ino
@@ -7,43 +7,33 @@
 #include <OneWire.h>
 #include <DallasTemperature.h>
 #include "EEPROM.h"
+#include "DS18B20_lib.h"
 
-hw_timer_t * timer = NULL;
+hw_timer_t *timer = NULL;
 volatile SemaphoreHandle_t timerSemaphore;
 portMUX_TYPE timerMux = portMUX_INITIALIZER_UNLOCKED;
 volatile uint32_t isrCounter = 0;
 volatile uint32_t lastIsrAt = 0;
 
-float maxTemp = -50;
-float minTemp = 100;
-
-
-
-String serverAddress = "https://intern.kluchens.eu/";
-int port = 80;
+DS18B20_lib sensor_array[] = {
+    DS18B20_lib(5),
+    DS18B20_lib(14),
+    DS18B20_lib(13),
+    DS18B20_lib(15),
+    DS18B20_lib(27),
+};
 
 WiFiMulti wifi;
-int status = WL_IDLE_STATUS;
-String response;
-int statusCode = 0;
 
-int pin = 5;               // pin 5
-OneWire  oneWire(pin);
-DallasTemperature sensors(&oneWire);
+String serverAddress = "http://TMS.Server.org/";
+int port = 80;
 
-uint8_t measurementCount = 0;
-float temp_temperature = 0;
-
-
-
-
-
-char str[50],ssid[30],key[30];
+char str[50], ssid[30], key[30];
 int state = 0;
 int matches = 0;
 
-
-void IRAM_ATTR onTimer(){
+void IRAM_ATTR onTimer()
+{
   portENTER_CRITICAL_ISR(&timerMux);
   isrCounter++;
   lastIsrAt = millis();
@@ -51,22 +41,24 @@ void IRAM_ATTR onTimer(){
   xSemaphoreGiveFromISR(timerSemaphore, NULL);
 }
 
-void setup() {
+void setup()
+{
   WRITE_PERI_REG(RTC_CNTL_BROWN_OUT_REG, 0);
   delay(1000);
- Serial.begin(115200);
-  if (!EEPROM.begin(64)) {
+  Serial.begin(115200);
+  if (!EEPROM.begin(64))
+  {
     Serial.println("Failed to initialise EEPROM");
     Serial.println("Restarting...");
     delay(1000);
     ESP.restart();
   }
-  for(int i = 0;i<20;i++)
+  for (int i = 0; i < 20; i++)
   {
     ssid[i] = 0;
     key[i] = 0;
   }
-  for(int i = 0;i<50;i++)
+  for (int i = 0; i < 50; i++)
   {
     str[i] = 0;
   }
@@ -74,127 +66,87 @@ void setup() {
   EEPROM.readString(0).toCharArray(__ssid, sizeof(__ssid));
   char __key[30];
   EEPROM.readString(30).toCharArray(__key, sizeof(__key));
-  wifi.addAP(__ssid,__key);
+  wifi.addAP(__ssid, __key);
   timerSemaphore = xSemaphoreCreateBinary();
   timer = timerBegin(0, 80, true);
   timerAttachInterrupt(timer, &onTimer, true);
   int timerInSeconds = 30;
-  int MiliSeconds = 1000* timerInSeconds;
-  int MicroSeconds = 1000* MiliSeconds;
+  int MiliSeconds = 1000 * timerInSeconds;
+  int MicroSeconds = 1000 * MiliSeconds;
   timerAlarmWrite(timer, MicroSeconds, true);
   timerAlarmEnable(timer);
-  sensors.begin();
 }
 
-void loop() {
-  switch( state )
+void loop()
+{
+  switch (state)
   {
-    case 0:
+  case 0:
     TemperatureMeasurement();
     break;
-    case 1:
+  case 1:
     PrepareConfig();
     break;
-    case 2:
+  case 2:
     WaitForSaveConfirmation();
     break;
   }
-  if(Serial.available() && state == 0)
-  state = 1;
-    
-    delay(1000);
-    //################## CALCS
- 
+  if (Serial.available() && state == 0)
+    state = 1;
+
+  delay(100);
+  //################## CALCS
 }
 
 void TemperatureMeasurement()
 {
-   float temperature = readTemp();
-  if (temperature > -50.0f && temperature < 150.0f) {
-           if(minTemp > temperature) {minTemp = temperature;}
-        if(maxTemp < temperature) {maxTemp = temperature;}
-       if(measurementCount == 0)
-       {
-          temp_temperature = (float)temperature;
-       } else {
-        Serial.println("Temperature read success: "+(String)((float)temp_temperature)); 
-          if(measurementCount > 1) {
-            temp_temperature = (((float)(measurementCount-1)*temp_temperature) + (float)temperature) / measurementCount;
-          } else {
-            if(measurementCount == 0)
-            {
-              temp_temperature = (float)temperature;
-            }
-          }
-      }
-    measurementCount++;  
-    } else {
-    Serial.print("Reading temperature failed"); 
+  for (int i = 0; i < sizeof(sensor_array)/sizeof(sensor_array[0]); i++)
+  {
+    sensor_array[i].MeasureTemp();
   }
-  if (xSemaphoreTake(timerSemaphore, 0) == pdTRUE){
+  if (xSemaphoreTake(timerSemaphore, 0) == pdTRUE)
+  {
     uint32_t isrCount = 0, isrTime = 0;
     portENTER_CRITICAL(&timerMux);
     isrCount = isrCounter;
     isrTime = lastIsrAt;
     portEXIT_CRITICAL(&timerMux);
-    if((wifi.run() == WL_CONNECTED)) {
-      HTTPClient http;
-      http.begin(serverAddress); //HTTP
-      http.addHeader("Content-Type","application/x-www-form-urlencoded");
-      int httpCode = http.POST(
-        "AVG_Temperature="+(String)temp_temperature+"&"+
-        "Max_Temperature="+(String)maxTemp+"&"+
-        "Min_Temperature="+(String)minTemp+"&"+
-        "MAC="+WiFi.macAddress()+"&"+
-        "Password="+"ESPtrzecie"
-        );
-      if(httpCode > 0) {
-              Serial.printf("[HTTP] GET... code: %d\n", httpCode);
-              if(httpCode == HTTP_CODE_OK) {
-                  String payload = http.getString();
-                  Serial.println(payload);
-              }
-          } else {
-              Serial.printf("[HTTP] GET... failed, error: %s\n", http.errorToString(httpCode).c_str());
-          }
-        http.end();
-        Serial.print("Status code: ");
-        Serial.println(httpCode);
-        Serial.print("Response: ");
-        Serial.println(response);
-        temp_temperature = 0;
-        measurementCount = 0;
-
-        maxTemp = -50;
-        minTemp = 100;
-    } else {
-    Serial.println("WIFI NOT CONNECTED, REBOOT");
-    delay(300);
-    ESP.restart();
+    if ((wifi.run() == WL_CONNECTED))
+    {
+      for (int i = 0; i < sizeof(sensor_array)/sizeof(sensor_array[0]); i++)
+      {
+        ReadingDatagram datagram = sensor_array[i].CurrentDatagram();
+        PushDataToServer(datagram);
+        sensor_array[i].ResetTemperature();
+      }
+    }
+    else
+    {
+      Serial.println("WIFI NOT CONNECTED, REBOOT");
+      delay(300);
+      ESP.restart();
+    }
   }
-  }
-}
-float readTemp()
-{
-  sensors.requestTemperatures(); 
-  return sensors.getTempCByIndex(0);
 }
 
 bool PrepareConfig()
 {
-  for (byte i=0; Serial.available(); i++){
-     str[i] = Serial.read();
-  }     
-  matches = sscanf (str,"@WIFI %s %s", &ssid,&key);
-  Serial.print("Matched ");Serial.print(matches);Serial.println(" credentials");
-  if(matches == 1)
+  for (byte i = 0; Serial.available(); i++)
+  {
+    str[i] = Serial.read();
+  }
+  matches = sscanf(str, "@WIFI %s %s", &ssid, &key);
+  Serial.print("Matched ");
+  Serial.print(matches);
+  Serial.println(" credentials");
+  if (matches == 1)
   {
     Serial.print("Wifi SSID is: ");
     Serial.println(ssid);
     state = 2;
     return true;
-  }  
-  if(matches == 2)
+  }
+  if (matches == 2)
   {
     Serial.print("Wifi Key is: ");
     Serial.println(key);
@@ -210,37 +162,37 @@ bool PrepareConfig()
 void WaitForSaveConfirmation()
 {
   Serial.println("To save changes print @SAVE, to reject print @REJECT");
-  if(matches == 1)
+  if (matches == 1)
   {
     Serial.print("Wifi SSID is: ");
     Serial.println(ssid);
-  }  
-  if(matches == 2)
+  }
+  if (matches == 2)
   {
     Serial.print("Wifi Key is: ");
     Serial.println(key);
     Serial.print("Wifi SSID is: ");
     Serial.println(ssid);
   }
-  if(Serial.available())
+  if (Serial.available())
   {
     String action = Serial.readString();
-    if(action == "@REJECT")
+    if (action == "@REJECT")
     {
       Serial.println("Reverting Changes");
-      for(int i = 0;i<20;i++)
+      for (int i = 0; i < 20; i++)
       {
         ssid[i] = 0;
         key[i] = 0;
       }
-      for(int i = 0;i<50;i++)
+      for (int i = 0; i < 50; i++)
       {
         str[i] = 0;
       }
     }
-    if(action == "@SAVE")
+    if (action == "@SAVE")
     {
-      Serial.println("Saving Changes");      
+      Serial.println("Saving Changes");
       CommitChanges();
     }
     state = 0;
@@ -250,21 +202,59 @@ void WaitForSaveConfirmation()
 
 void CommitChanges()
 {
-  for(int x = 0;x<64;x++)
-  {EEPROM.write(x,0);}
-  EEPROM.writeString(0,ssid);
+  for (int x = 0; x < 64; x++)
+  {
+    EEPROM.write(x, 0);
+  }
+  EEPROM.writeString(0, ssid);
   yield();
-  EEPROM.writeString(sizeof(ssid),key);
+  EEPROM.writeString(sizeof(ssid), key);
   yield();
   EEPROM.commit();
-  for(int i = 0;i<20;i++)
+  for (int i = 0; i < 20; i++)
   {
     ssid[i] = 0;
     key[i] = 0;
   }
-  for(int i = 0;i<50;i++)
+  for (int i = 0; i < 50; i++)
   {
     str[i] = 0;
   }
   Serial.println("Config Saved");
+}
+
+bool PushDataToServer(ReadingDatagram data)
+{
+  int status = WL_IDLE_STATUS;
+  String response;
+  int statusCode = 0;
+  HTTPClient http;
+  http.begin(serverAddress); //HTTP
+  http.addHeader("Content-Type", "application/x-www-form-urlencoded");
+  int httpCode = http.POST(
+      "AVG_Temperature=" + (String)data.AVG_Temperature + "&" +
+      "Max_Temperature=" + (String)data.Max_Temperature + "&" +
+      "Min_Temperature=" + (String)data.Min_Temperature + "&" +
+      "MAC=" + WiFi.macAddress() + "&" +
+      "Password=" + "ESPtrzecie" + "&" +
+      "ApiVersion=" + "1.1" + "&" +
+      "Sensor_PIN=" + (String)data.PIN);
+  if (httpCode > 0)
+  {
+    Serial.printf("[HTTP] GET... code: %d\n", httpCode);
+    if (httpCode == HTTP_CODE_OK)
+    {
+      String payload = http.getString();
+      Serial.println(payload);
+    }
+  }
+  else
+  {
+    Serial.printf("[HTTP] GET... failed, error: %s\n", http.errorToString(httpCode).c_str());
+  }
+  http.end();
+  Serial.print("Status code: ");
+  Serial.println(httpCode);
+  Serial.print("Response: ");
+  Serial.println(response);
 }
